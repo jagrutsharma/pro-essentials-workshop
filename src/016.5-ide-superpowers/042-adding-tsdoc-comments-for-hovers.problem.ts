84f1d122055c7da6571b40b4ea89d35b19fbb801
Jagrut Sharma <jagrutsharma@gmail.com>
2025-08-21 12:16:47 -0700

    Adding function docs

--- src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
+++ src/016.5-ide-superpowers/042-adding-tsdoc-comments-for-hovers.problem.ts
@@ -1,3 +1,15 @@
+/**
+ * Add two numbers together
+ * @param a - The first number
+ * @param b - The second number
+ * @returns The sum of the two numbers
+ * 
+ * @example
+ * myFunction(1, 2)
+ */
 const myFunction = (a: number, b: number) => {
   return a + b;
 };
+
+const result = myFunction(1, 2);
+console.log(result);
